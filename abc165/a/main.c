3c88c2e6236b503c15b04d782d8c986c1ee7e181
utsubo256 <96920121+utsubo256@users.noreply.github.com>
2026-06-27 17:03:55 +0900

    Simplify ABC165 A C solution

--- abc165/a/main.c
+++ abc165/a/main.c
@@ -3,12 +3,9 @@
 int main(void) {
   int k, a, b;
   scanf("%d %d %d", &k, &a, &b);
-  for (int i = a; i <= b; i++) {
-    if (i % k == 0) {
-      puts("OK");
-      return 0;
-    }
-  }
-  puts("NG");
+  // (b / k) * k part finds the largest multiple of k that is less than or equal
+  puts((b / k) * k >= a ? "OK" : "NG");
+  // Alternative solution: find the largest multiple of k that is less than or
+  // equal to b. puts(((a + k - 1) / k) * k <= b ? "OK" : "NG");
   return 0;
 }
