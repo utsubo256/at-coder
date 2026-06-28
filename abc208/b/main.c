85936c98367cc6506a29e9dd5732668d2d44f41b
utsubo256 <96920121+utsubo256@users.noreply.github.com>
2026-06-28 19:13:08 +0900

    Simplify ABC208 B C solution

--- abc208/b/main.c
+++ abc208/b/main.c
@@ -2,25 +2,20 @@
 
 int factorial(int n) {
   int total = 1;
-  if (n == 0) {
-    return total;
-  }
-  for (int i = n; i >= 1; i--) {
-    total = total * i;
+  for (int i = 2; i <= n; i++) {
+    total *= i;
   }
   return total;
 }
 
 int main(void) {
-  int p, coin, nums = 0;
+  int p, coins = 0;
   scanf("%d", &p);
   for (int i = 10; i >= 1; i--) {
-    coin = factorial(i);
-    while (p >= coin) {
-      p -= coin;
-      nums++;
-    }
+    int coin = factorial(i);
+    coins += p / coin;
+    p %= coin;
   }
-  printf("%d\n", nums);
+  printf("%d\n", coins);
   return 0;
 }
