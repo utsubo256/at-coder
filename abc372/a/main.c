251247be4c65b8799b3e94d4a38dc980e5726665
utsubo256 <96920121+utsubo256@users.noreply.github.com>
2026-07-01 19:18:46 +0900

    Solve ABC372 A in C

---
+++ abc372/a/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+  char s[101];
+  scanf("%s", s);
+  for (int i = 0; s[i] != '\0'; i++) {
+    if (s[i] != '.') {
+      putchar(s[i]);
+    }
+  }
+  putchar('\n');
+
+  return 0;
+}
